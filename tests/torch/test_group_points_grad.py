f220a486dcc44f00add5d4cdeb4423bdb9c61d57
wujiadi <wujiadi1@huawei.com>
2024-07-26 08:32:26 +0000

    !304 group_points_grad算子UT泛化 Merge pull request !304 from wujiadi/group_points_grad_ut

--- tests/torch/test_group_points_grad.py
+++ tests/torch/test_group_points_grad.py
@@ -28,24 +28,31 @@ class TestGroupPointsGrad(TestCase):
 
     @unittest.skipIf(DEVICE_NAME != 'Ascend910B', "OP `GroupPointsGrad` is only supported on 910B, skip this ut!")
     def test_group_points_grad(self):
-        B = 16
-        C = 512
-        N = 64
-        npoints = 16
-        nsample = 32
+        np.random.seed(50051)
+        B_list = [16, 32, 64]
+        C_list = [16, 31, 32, 35, 64, 512]
+        N_list = [64]
+        npoints_list = [16, 100]
+        nsample_list = [32, 50]
 
-        np_grad_out = np.random.rand(B, C, npoints, nsample).astype(np.float32)
-        np_indices = np.random.randint(0, N, (B, npoints, nsample)).astype(np.int32)
-        np_grad_features = np.zeros((B, C, N)).astype(np.float32)
+        for B in B_list:
+            for C in C_list:
+                for N in N_list:
+                    for npoints in npoints_list:
+                        for nsample in nsample_list:
+                            np_grad_out = np.random.rand(B, C, npoints, nsample).astype(np.float32)
+                            np_indices = np.random.randint(0, N, (B, npoints, nsample)).astype(np.int32)
+                            np_grad_features = np.zeros((B, C, N)).astype(np.float32)
 
-        torch_grad_out = torch.from_numpy(np_grad_out).npu()
-        torch_indices = torch.from_numpy(np_indices).npu()
+                            torch_grad_out = torch.from_numpy(np_grad_out).npu()
+                            torch_indices = torch.from_numpy(np_indices).npu()
 
-        golden_grad_features = self.golden_group_points_grad(
-            np_grad_out, np_indices, np_grad_features, B, npoints, nsample)
-        npu_grad_features = ads_c.group_points_backward(torch_grad_out, torch_indices, B, C, N, npoints, nsample)
+                            golden_grad_features = self.golden_group_points_grad(
+                                np_grad_out, np_indices, np_grad_features, B, npoints, nsample)
+                            npu_grad_features = ads_c.group_points_backward(torch_grad_out, torch_indices, B, C, N,
+                                                                            npoints, nsample)
 
-        self.assertRtolEqual(golden_grad_features, npu_grad_features.cpu().numpy())
+                            self.assertRtolEqual(golden_grad_features, npu_grad_features.cpu().numpy())
 
 
 if __name__ == "__main__":
