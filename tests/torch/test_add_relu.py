703bb48e5f64ec7c929b6ac0fe5bbf416c2ab01d
王晓歆 <wangxiaoxin7@huawei.com>
2024-08-06 06:38:34 +0000

    !317 新增AddRelu测试用例 Merge pull request !317 from 王晓歆/addrelu

--- tests/torch/test_add_relu.py
+++ tests/torch/test_add_relu.py
@@ -30,6 +30,26 @@ class TestPointsInBox(TestCase):
         cpu_result = F.relu(x + y)
         x = mx_driving.common.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
         self.assertRtolEqual(x, cpu_result.numpy())
+    
+    @unittest.skipIf(DEVICE_NAME != 'Ascend910B', "OP `AddRelu` is only supported on 910B, skip this ut!")
+    def test_npu_add_relu_fp16_large_number(self, device="npu"):
+        x = np.random.uniform(1, 1, [18, 256, 232, 400]).astype(np.float16)
+        x = torch.from_numpy(x)
+        y = np.random.uniform(2.0, 2.0, [18, 256, 232, 400]).astype(np.float16)
+        y = torch.from_numpy(y)
+        cpu_result = F.relu(x.float() + y.float())
+        x = mx_driving.common.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
+        self.assertRtolEqual(x, cpu_result.half().numpy())
+    
+    @unittest.skipIf(DEVICE_NAME != 'Ascend910B', "OP `AddRelu` is only supported on 910B, skip this ut!")
+    def test_npu_add_relu_fp16_small_case(self, device="npu"):
+        x = np.random.uniform(1, 1, [18]).astype(np.float16)
+        x = torch.from_numpy(x)
+        y = np.random.uniform(2.0, 2.0, [18]).astype(np.float16)
+        y = torch.from_numpy(y)
+        cpu_result = F.relu(x.float() + y.float())
+        x = mx_driving.common.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
+        self.assertRtolEqual(x, cpu_result.half().numpy())
 
 
 if __name__ == "__main__":
