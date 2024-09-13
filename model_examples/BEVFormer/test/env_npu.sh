cb0510a0deeb9817152eb85bdea3dbab71d8452d
刘哲续 <liuzhexu1@huawei.com>
2024-09-13 01:18:08 +0000

    update model_examples/BEVFormer/test/env_npu.sh.
    Signed-off-by: 刘哲续 <liuzhexu1@huawei.com>

--- model_examples/BEVFormer/test/env_npu.sh
+++ model_examples/BEVFormer/test/env_npu.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-CANN_INSTALL_PATH_CONF='/etc/Ascend/ascend_cann_install.info'
-
-if [ -f $CANN_INSTALL_PATH_CONF ]; then
-    CANN_INSTALL_PATH=$(cat $CANN_INSTALL_PATH_CONF | grep Install_Path | cut -d "=" -f 2)
-else
-    CANN_INSTALL_PATH="/usr/local/Ascend"
-fi
-
-if [ -d ${CANN_INSTALL_PATH}/ascend-toolkit/latest ]; then
-    source ${CANN_INSTALL_PATH}/ascend-toolkit/set_env.sh
-else
-    source ${CANN_INSTALL_PATH}/nnae/set_env.sh
-fi
 msnpureport -g error -d 0
 msnpureport -g error -d 1
 msnpureport -g error -d 2
@@ -29,10 +16,12 @@ export ASCEND_SLOG_PRINT_TO_STDOUT=0
 export ASCEND_GLOBAL_LOG_LEVEL=3
 #设置Event日志开启标志,0-关闭/1-开启
 export ASCEND_GLOBAL_EVENT_ENABLE=0
-#设置是否开启taskque,0-关闭/1-开启
-export TASK_QUEUE_ENABLE=1
+#设置是否开启taskque,0-关闭/1-开启/2-流水优化
+export TASK_QUEUE_ENABLE=2
 #设置是否开启combined标志,0-关闭/1-开启
 export COMBINED_ENABLE=1
+#设置是否开启均匀绑核,0-关闭/1-开启
+export CPU_AFFINITY_CONF=1
 #HCCL白名单开关,1-关闭/0-开启
 export HCCL_WHITELIST_DISABLE=1
 export HCCL_IF_IP=$(hostname -I |awk '{print $1}')
