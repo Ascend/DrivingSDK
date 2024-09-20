d111b055bdfca5f91a7e5bea08323c25978dee49
刘哲续 <liuzhexu1@huawei.com>
2024-09-20 01:25:29 +0000

    update model_examples/BEVFormer/projects/mmdet3d_plugin/__init__.py.
    Signed-off-by: 刘哲续 <liuzhexu1@huawei.com>

--- model_examples/BEVFormer/projects/mmdet3d_plugin/__init__.py
+++ model_examples/BEVFormer/projects/mmdet3d_plugin/__init__.py
@@ -1,10 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 # Copyright 2024 Huawei Technologies Co., Ltd
-import models.utils
-import bevformer
-import dd3d
-from .models.opt.adamw import AdamW2
-
 from .core.bbox.assigners.hungarian_assigner_3d import HungarianAssigner3D
 from .core.bbox.coders.nms_free_coder import NMSFreeCoder
 from .core.bbox.match_costs import BBox3DL1Cost
@@ -12,3 +7,8 @@ from .core.evaluation.eval_hooks import CustomDistEvalHook
 from .datasets.pipelines import (
   PhotoMetricDistortionMultiViewImage, PadMultiViewImage, 
   NormalizeMultiviewImage, CustomCollect3D)
+
+from .models.utils import *
+from .models.opt.adamw import AdamW2
+from .bevformer import *
+from .dd3d import *
