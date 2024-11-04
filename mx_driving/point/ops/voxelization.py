25c67a4982e77fa6e24a9b3a9f2eaa68b525aa1b
Mingkai Chan <chenmingkai3@huawei.com>
2024-11-04 07:35:48 +0000

    !543 [Feature]修改voxelization参数配置 Merge pull request !543 from Mingkai Chan/voxelization

--- mx_driving/point/ops/voxelization.py
+++ mx_driving/point/ops/voxelization.py
@@ -2,10 +2,11 @@
 Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved.
 """
 
-from typing import Union, Tuple
 import torch
 from torch.autograd import Function
 from torch.nn import Module
+from torch.nn.modules.utils import _pair
+
 import mx_driving._C
 
 
@@ -50,18 +51,18 @@ voxelization = _Voxelization.apply
 
 
 class Voxelization(torch.nn.Module):
-    def __init__(
-        self, voxel_size, point_cloud_range, max_num_points: int, max_voxels: int = 20000, deterministic: bool = True
-    ):
+    def __init__(self, voxel_size, point_cloud_range, max_num_points, max_voxels=20000, deterministic=True):
         super().__init__()
 
         self.voxel_size = voxel_size
         self.point_cloud_range = point_cloud_range
         self.max_num_points = max_num_points
         self.max_voxels = max_voxels
+        self.max_voxels = max_voxels if isinstance(max_voxels, tuple) else _pair(max_voxels)
         self.deterministic = deterministic
 
     def forward(self, points: torch.Tensor):
+        max_voxels = self.max_voxels[0] if self.training else self.max_voxels[1]
         return voxelization(
-            points, self.voxel_size, self.point_cloud_range, self.max_num_points, self.max_voxels, self.deterministic
+            points, self.voxel_size, self.point_cloud_range, self.max_num_points, max_voxels, self.deterministic
         )
