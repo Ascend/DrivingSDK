1d0c83bf00bc03e6e295914e4b8af0b241ff60cc
王晓歆 <wangxiaoxin7@huawei.com>
2024-12-04 09:21:46 +0000

    !677 update add_relu doc. Merge pull request !677 from 王晓歆/addreludocc

--- tests/torch/test_add_relu.py
+++ tests/torch/test_add_relu.py
@@ -18,8 +18,10 @@ class TestPointsInBox(TestCase):
         y = np.random.uniform(2.0, 2.0, [1, 100, 3]).astype(np.float32)
         y = torch.from_numpy(y)
         cpu_result = F.relu(x + y)
-        x = mx_driving.fused.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
-        self.assertRtolEqual(x, cpu_result.numpy())
+        result = mx_driving.fused.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
+        self.assertRtolEqual(result, cpu_result.numpy())
+        result = mx_driving.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
+        self.assertRtolEqual(result, cpu_result.numpy())
     
     @unittest.skipIf(DEVICE_NAME != 'Ascend910B', "OP `AddRelu` is only supported on 910B, skip this ut!")
     def test_npu_add_relu_large_number(self, device="npu"):
@@ -28,8 +30,10 @@ class TestPointsInBox(TestCase):
         y = np.random.uniform(2.0, 2.0, [18, 256, 232, 400]).astype(np.float32)
         y = torch.from_numpy(y)
         cpu_result = F.relu(x + y)
-        x = mx_driving.fused.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
-        self.assertRtolEqual(x, cpu_result.numpy())
+        result = mx_driving.fused.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
+        self.assertRtolEqual(result, cpu_result.numpy())
+        result = mx_driving.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
+        self.assertRtolEqual(result, cpu_result.numpy())
     
     @unittest.skipIf(DEVICE_NAME != 'Ascend910B', "OP `AddRelu` is only supported on 910B, skip this ut!")
     def test_npu_add_relu_fp16_large_number(self, device="npu"):
@@ -38,8 +42,10 @@ class TestPointsInBox(TestCase):
         y = np.random.uniform(2.0, 2.0, [18, 256, 232, 400]).astype(np.float16)
         y = torch.from_numpy(y)
         cpu_result = F.relu(x.float() + y.float())
-        x = mx_driving.fused.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
-        self.assertRtolEqual(x, cpu_result.half().numpy())
+        result = mx_driving.fused.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
+        self.assertRtolEqual(result, cpu_result.half().numpy())
+        result = mx_driving.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
+        self.assertRtolEqual(result, cpu_result.half().numpy())
     
     @unittest.skipIf(DEVICE_NAME != 'Ascend910B', "OP `AddRelu` is only supported on 910B, skip this ut!")
     def test_npu_add_relu_fp16_small_case(self, device="npu"):
@@ -48,8 +54,10 @@ class TestPointsInBox(TestCase):
         y = np.random.uniform(2.0, 2.0, [18]).astype(np.float16)
         y = torch.from_numpy(y)
         cpu_result = F.relu(x.float() + y.float())
-        x = mx_driving.fused.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
-        self.assertRtolEqual(x, cpu_result.half().numpy())
+        result = mx_driving.fused.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
+        self.assertRtolEqual(result, cpu_result.half().numpy())
+        result = mx_driving.npu_add_relu(x.npu(), y.npu()).cpu().numpy()
+        self.assertRtolEqual(result, cpu_result.half().numpy())
 
 
 if __name__ == "__main__":
