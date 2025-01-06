b8d643730c3c91d126a421736307c030d8fb7279
zhangchenrui <zhangchenrui2@huawei.com>
2025-01-06 01:16:42 +0000

    !866 添加SurroundOcc模型patch Merge pull request !866 from zhangchenrui/master

--- model_examples/SurroundOcc/projects/mmdet3d_plugin/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .nuscenes_dataset import CustomNuScenesDataset
-from .nuscenes_occupancy_dataset import CustomNuScenesOccDataset
-from .builder import custom_build_dataset
-
-__all__ = [
-    'CustomNuScenesDataset', 'CustomNuScenesOccDataset'
-]
--- model_examples/SurroundOcc/projects/mmdet3d_plugin/surroundocc/detectors/__init__.py
+++
@@ -1,1 +0,0 @@
-from .surroundocc import SurroundOcc
