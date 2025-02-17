a57537fa0747bd1dacdf787c4074be512307647e
朱维琛 <zhuweichen2@huawei.com>
2025-02-17 07:03:28 +0000

    !966 enhance patcher Merge pull request !966 from 朱维琛/enhance_patcher

--- mx_driving/patcher/profiler.py
+++ mx_driving/patcher/profiler.py
@@ -62,11 +62,52 @@ def profiler(runner: ModuleType, options: Dict):
         ) as prof:
             for idx, data_batch in enumerate(self.data_loader):
                 self.run_iter(idx, data_batch)
+                prof.step()
 
         self.runner.call_hook("after_train_epoch")
         self.runner._epoch += 1
+        
+    def run(self) -> None:
+        self.runner.call_hook('before_train')
+        self.runner.call_hook('before_train_epoch')
+        if self._iter > 0:
+            print_log(
+                f'Advance dataloader {self._iter} steps to skip data '
+                'that has already been trained',
+                logger='current',
+                level=logging.WARNING)
+            for _ in range(self._iter):
+                next(self.dataloader_iterator)
+        with torch_npu.profiler.profile(
+            activities=activities,
+            with_stack=level == 2,
+            record_shapes=level > 0,
+            profile_memory=level == 2,
+            schedule=torch_npu.profiler.schedule(wait=1, warmup=1, active=1, repeat=1, skip_first=20),
+            experimental_config=torch_npu.profiler._ExperimentalConfig(profiler_level=profiler_level),
+            on_trace_ready=torch_npu.profiler.tensorboard_trace_handler(path),
+        ) as prof:
+            while self._iter < self._max_iters and not self.stop_training:
+                self.runner.model.train()
+
+                data_batch = next(self.dataloader_iterator)
+                self.run_iter(data_batch)
+                prof.step()
+
+                self._decide_current_val_interval()
+                if (self.runner.val_loop is not None
+                        and self._iter >= self.val_begin
+                        and (self._iter % self.val_interval == 0
+                            or self._iter == self._max_iters)):
+                    self.runner.val_loop.run()
+
+        self.runner.call_hook('after_train_epoch')
+        self.runner.call_hook('after_train')
+        return self.runner.model
 
     if hasattr(runner, "EpochBasedRunner"):
         runner.EpochBasedRunner.train = train
-    elif hasattr(runner, "EpochBasedTrainLoop"):
+    if hasattr(runner, "EpochBasedTrainLoop"):
         runner.EpochBasedTrainLoop.run_epoch = run_epoch
+    if hasattr(runner, "IterBasedTrainLoop"):
+        runner.IterBasedTrainLoop.run = run
