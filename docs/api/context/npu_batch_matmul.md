a39c1b93df95a0da189dd931d51b7ec2b69d0eac
王晓歆 <wangxiaoxin7@huawei.com>
2025-02-25 07:31:09 +0000

    !982 update doc and add test. Merge pull request !982 from 王晓歆/submbugfix

--- docs/api/context/npu_batch_matmul.md
+++ docs/api/context/npu_batch_matmul.md
@@ -7,13 +7,14 @@ mx_driving.npu_batch_matmul(Tensor projection_mat, Tensor pts_extend) -> Tensor
 ### 功能描述
 
 ### 参数说明
-- `projection_mat(Tensor)`：投影矩阵，数据类型为`float32`。Shape为6维，维度必须形如`B, N, 1, 1, 4, 4`。
-- `pts_extend(Tensor)`：所有点的特征，数据类型为`float32`。Shape为6维，维度必须形如`B, 1, M, P, 1, 4`。
+- `projection_mat(Tensor)`：投影矩阵，数据类型为`float32`。Shape为4维以上，最后两维需要是`4, 4`或`3, 3`，且和pts_extend互相可广播。
+- `pts_extend(Tensor)`：所有点的特征，数据类型为`float32`。Shape为4维以上，最后两维需要是`1, 4`、`1, 3`等，且和projection_mat互相可广播。
 ### 返回值
-- `output`：矩阵乘结果，数据类型为`float32`。Shape为`B, N, M, P, 4, 4`。
+- `output`：矩阵乘结果，数据类型为`float32`。
 ### 支持的型号
 - Atlas A2 训练系列产品
 ### 调用示例
+输入是6维
 ```python
 import numpy as np
 import torch, torch_npu
@@ -25,6 +26,19 @@ pts_extend2_fused = pts_extend2_fused[:, None, :, :, None, :].contiguous()
 projection_mat_fused.requires_grad=True
 pts_extend2_fused.requires_grad=True        
 result = mx_driving.npu_batch_matmul(projection_mat_fused, pts_extend2_fused)
+grad = torch.ones_like(result)
+result.backward(grad)
+```
+输入是4维
+```python
+import numpy as np
+import torch, torch_npu
+import mx_driving
+projection_mat =torch.randn((6, 6, 4, 4)).npu()
+pts_extend =torch.randn(6, 1220, 1, 4).npu()
+projection_mat.requires_grad=True
+pts_extend.requires_grad=True        
+result = mx_driving.npu_batch_matmul(projection_mat, pts_extend)
 grad = torch.ones_like(result)
 result.backward(grad)
 ```
--- tests/torch/test_npu_batch_matmul.py
+++ tests/torch/test_npu_batch_matmul.py
@@ -22,7 +22,7 @@ def gen_former_npu_outputs(projection_mat, pts_extend):
 
 
 class TestBatchMatmul(TestCase):  
-    def test_npu_batch_matmul(self, device="npu"):
+    def test_npu_batch_matmul_sixdim(self, device="npu"):
         projection_mat, pts_extend = gen_inputs([6, 6, 4, 4], [6, 1220, 13, 4], np.float32)
         projection_mat_fused = projection_mat.detach()
         pts_extend2_fused = pts_extend.detach()
@@ -39,9 +39,104 @@ class TestBatchMatmul(TestCase):
         result = mx_driving.npu_batch_matmul(projection_mat_fused, pts_extend2_fused)
         grad = torch.ones_like(result)
         result.backward(grad)
-        print("result", result.shape)
-        print("former_npu_result", former_npu_result.shape)
 
         self.assertRtolEqual(result.detach().cpu().numpy(), former_npu_result.detach().cpu().numpy())
+    
+    def test_npu_batch_matmul_four_dim(self, device="npu"):
+        projection_mat, pts_extend = gen_inputs([6, 1, 4, 4], [6, 1220, 4, 1], np.float32)
+        projection_mat_fused = projection_mat.detach()
+        pts_extend2_fused = pts_extend.detach()
+        projection_mat.requires_grad = True
+        pts_extend.requires_grad = True
+        former_npu_result = torch.matmul(projection_mat_fused, pts_extend)
+        grad = torch.ones_like(former_npu_result)
+        former_npu_result.backward(grad)  
+        
+        projection_mat_fused.requires_grad = True
+        pts_extend2_fused.requires_grad = True
+        pts_extend2_fused = pts_extend2_fused.transpose(3, 2).contiguous()
+        result = mx_driving.npu_batch_matmul(projection_mat_fused, pts_extend2_fused)
+        grad = torch.ones_like(result)
+        result.backward(grad)
+
+        self.assertRtolEqual(result.detach().cpu().numpy(), former_npu_result.detach().cpu().numpy())
+    
+    def test_npu_batch_matmul_none_brodcast(self, device="npu"):
+        projection_mat, pts_extend = gen_inputs([6, 1220, 4, 4], [6, 1220, 4, 1], np.float32)
+        projection_mat_fused = projection_mat.detach()
+        pts_extend2_fused = pts_extend.detach()
+        projection_mat.requires_grad = True
+        pts_extend.requires_grad = True      
+        former_npu_result = torch.matmul(projection_mat, pts_extend)
+        grad = torch.ones_like(former_npu_result)
+        former_npu_result.backward(grad)  
+
+        projection_mat_fused.requires_grad = True
+        pts_extend2_fused.requires_grad = True 
+        pts_extend2_fused = pts_extend2_fused.transpose(3, 2).contiguous()
+        result = mx_driving.npu_batch_matmul(projection_mat_fused, pts_extend2_fused)
+        grad = torch.ones_like(result)
+        result.backward(grad)
+
+        self.assertRtolEqual(result.detach().cpu().numpy(), former_npu_result.detach().cpu().numpy())
+    
+    def test_npu_batch_matmul_need_brodcast(self, device="npu"):
+        projection_mat, pts_extend = gen_inputs([1, 1, 4, 4], [6, 1220, 4, 1], np.float32)
+        projection_mat_fused = projection_mat.detach()
+        pts_extend2_fused = pts_extend.detach()
+        projection_mat.requires_grad = True
+        pts_extend.requires_grad = True      
+        former_npu_result = torch.matmul(projection_mat, pts_extend)
+        grad = torch.ones_like(former_npu_result)
+        former_npu_result.backward(grad)  
+
+        projection_mat_fused.requires_grad = True
+        pts_extend2_fused.requires_grad = True  
+        pts_extend2_fused = pts_extend2_fused.transpose(3, 2).contiguous()   
+        result = mx_driving.npu_batch_matmul(projection_mat_fused, pts_extend2_fused)
+        grad = torch.ones_like(result)
+        result.backward(grad)
+
+        self.assertRtolEqual(result.detach().cpu().numpy(), former_npu_result.detach().cpu().numpy())
+    
+    def test_npu_batch_matmul_kernel_3(self, device="npu"):
+        projection_mat, pts_extend = gen_inputs([6, 6, 3, 3], [6, 1220, 13, 3], np.float32)
+        projection_mat_fused = projection_mat.detach()
+        pts_extend2_fused = pts_extend.detach()
+        projection_mat.requires_grad = True
+        pts_extend.requires_grad = True      
+        former_npu_result = gen_former_npu_outputs(projection_mat, pts_extend)
+        grad = torch.ones_like(former_npu_result)
+        former_npu_result.backward(grad)  
+
+        projection_mat_fused = projection_mat_fused[:, :, None, None].contiguous()
+        pts_extend2_fused = pts_extend2_fused[:, None, :, :, None, :].contiguous()
+        projection_mat_fused.requires_grad = True
+        pts_extend2_fused.requires_grad = True        
+        result = mx_driving.npu_batch_matmul(projection_mat_fused, pts_extend2_fused)
+        grad = torch.ones_like(result)
+        result.backward(grad)
+
+        self.assertRtolEqual(result.detach().cpu().numpy(), former_npu_result.detach().cpu().numpy())
+    
+    def test_npu_batch_matmul_kernel_3_dim_4(self, device="npu"):
+        projection_mat, pts_extend = gen_inputs([6, 1, 3, 3], [6, 1220, 3, 1], np.float32)
+        projection_mat_fused = projection_mat.detach()
+        pts_extend2_fused = pts_extend.detach()
+        projection_mat.requires_grad = True
+        pts_extend.requires_grad = True      
+        former_npu_result = torch.matmul(projection_mat, pts_extend)
+        grad = torch.ones_like(former_npu_result)
+        former_npu_result.backward(grad)  
+
+        projection_mat_fused.requires_grad = True
+        pts_extend2_fused.requires_grad = True
+        pts_extend2_fused = pts_extend2_fused.transpose(3, 2).contiguous()      
+        result = mx_driving.npu_batch_matmul(projection_mat_fused, pts_extend2_fused)
+        grad = torch.ones_like(result)
+        result.backward(grad)
+
+        self.assertRtolEqual(result.detach().cpu().numpy(), former_npu_result.detach().cpu().numpy())
+
 if __name__ == "__main__":
     run_tests()
