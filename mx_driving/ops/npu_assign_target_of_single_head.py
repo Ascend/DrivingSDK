b650b91ecf3e89afc052c035f2c94357ecdf4c8d
Sunshun-Yuan <yuansunshun@huawei.com>
2025-03-05 08:40:17 +0000

    !1052 LIBING codechek Merge pull request !1052 from Sunshun-Yuan/master

--- mx_driving/ops/npu_assign_target_of_single_head.py
+++ mx_driving/ops/npu_assign_target_of_single_head.py
@@ -31,7 +31,8 @@ class AssignTargetOfSingleHead(Function):
         norm_bbox=True,
         with_velocity=True,
         flip_angle=False,
-        max_objs=500):
+        max_objs=500,
+    ):
         output = mx_driving._C.npu_assign_target_of_single_head(
             boxes,
             cur_class_id,
@@ -45,33 +46,39 @@ class AssignTargetOfSingleHead(Function):
             norm_bbox,
             with_velocity,
             flip_angle,
-            max_objs)
+            max_objs,
+        )
         return output
 
 
-def npu_assign_target_of_single_head(boxes,
-                                     cur_class_id,
-                                     num_classes,
-                                     out_size_factor,
-                                     gaussian_overlap,
-                                     min_radius,
-                                     voxel_size,
-                                     pc_range,
-                                     feature_map_size,
-                                     norm_bbox=True,
-                                     with_velocity=True,
-                                     flip_angle=False,
-                                     max_objs=500):
-    return AssignTargetOfSingleHead.apply(boxes,
-                                          cur_class_id,
-                                          num_classes,
-                                          out_size_factor,
-                                          gaussian_overlap,
-                                          min_radius,
-                                          voxel_size,
-                                          pc_range,
-                                          feature_map_size,
-                                          norm_bbox,
-                                          with_velocity,
-                                          flip_angle,
-                                          max_objs)
+# pylint: disable=too-many-arguments,huawei-too-many-arguments
+def npu_assign_target_of_single_head(
+    boxes,
+    cur_class_id,
+    num_classes,
+    out_size_factor,
+    gaussian_overlap,
+    min_radius,
+    voxel_size,
+    pc_range,
+    feature_map_size,
+    norm_bbox=True,
+    with_velocity=True,
+    flip_angle=False,
+    max_objs=500,
+):
+    return AssignTargetOfSingleHead.apply(
+        boxes,
+        cur_class_id,
+        num_classes,
+        out_size_factor,
+        gaussian_overlap,
+        min_radius,
+        voxel_size,
+        pc_range,
+        feature_map_size,
+        norm_bbox,
+        with_velocity,
+        flip_angle,
+        max_objs,
+    )
