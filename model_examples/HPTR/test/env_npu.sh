6141cbc31ae828a8f8dcaf10627e9e2e0fb4f944
肖少宁 <xiaoshaoning@h-partners.com>
2025-03-12 07:48:59 +0000

    !1087 新增 HPTR 模型 Merge pull request !1087 from 肖少宁/master

---
+++ model_examples/HPTR/test/env_npu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+CANN_INSTALL_PATH_CONF='/etc/Ascend/ascend_cann_install.info'
+
+if [ -f CANN_INSTALL_PATH_CONF ]; then
+    CANN_INSTALL_PATH=$(cat $CANN_INSTALL_PATH_CONF | grep Install_Path | cut -d "=" -f 2)
+else
+    CANN_INSTALL_PATH="/usr/local/Ascend"
+fi
+
+if [ -d {CANN_INSTALL_PATH}/ascend-toolkit/latest ]; then
+    source ${CANN_INSTALL_PATH}/ascend-toolkit/set_env.sh
+else
+    source ${CANN_INSTALL_PATH}/nnae/set_env.sh
+fi
+
+#设置device侧日志登记为error
+msnpureport -g error -d 0
+msnpureport -g error -d 1
+msnpureport -g error -d 2
+msnpureport -g error -d 3
+msnpureport -g error -d 4
+msnpureport -g error -d 5
+msnpureport -g error -d 6
+msnpureport -g error -d 7
+#关闭Device侧Event日志
+msnpureport -e disable
+
+# 绑核
+export CPU_AFFINITY_CONF=1
+# 是否开启taskque
+export TASK_QUEUE_ENABLE=1
+# 设置shape数据缓存
+export HOST_CACHE_CAPACITY=20
+# 开启combined标志
+export COMBINED_ENABLE=1
+# 启用可扩展内存段分配策略
+export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
