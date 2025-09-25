167ee8be503a982124d3c47ac102627f33945982
ascend-robot <zhongyuanke@huawei.com>
2025-09-25 15:36:30 +0800

    fix panoocc libc10.so not found bug
    Co-authored-by: zhttjd<zhanghongtao33@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge master into master
    
    fix panoocc libc10.so not found bug
    
    Created-by: zhttjd
    Commit-by: zhttjd
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    修复test.py里的libc10.so文件无法找到的报错
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述:
    
    
    See merge request: Ascend/DrivingSDK!1652

--- model_examples/PanoOcc/migrate_to_ascend/test.py
+++ model_examples/PanoOcc/migrate_to_ascend/test.py
@@ -1,6 +1,8 @@
 # Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
 
 # pylint: disable=huawei-wrong-import-position, wrong-import-order
+import torch
+import torch_npu
 from mx_driving.patcher import patch_mmcv_version
 patch_mmcv_version('1.6.0')
 from migrate_to_ascend.patch_main import generate_patcher_builder
