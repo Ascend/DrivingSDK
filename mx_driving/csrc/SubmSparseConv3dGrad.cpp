306470fdbab7056583cc2cfbe34be70e85eae6d1
gitdzy <dingziyuan@huawei.com>
2026-01-15 09:03:32 +0800

    修改subm反向csrc，统一命名方式
    Co-authored-by: gitdzy<dingziyuan@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1856 merge submcsrc into master
    
    修改subm反向csrc，统一命名方式
    
    Created-by: gitdzy
    Commit-by: gitdzy
    Merged-by: ascend-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述:
    
    
    See merge request: Ascend/DrivingSDK!1856

--- mx_driving/csrc/SubmSparseConv3dGrad.cpp
+++ mx_driving/csrc/SubmSparseConv3dGrad.cpp
@@ -33,3 +33,40 @@ at::Tensor npu_subm_sparse_conv3d_grad(const at::Tensor& ouidx_offset, const at:
     EXEC_NPU_CMD(aclnnSubmSparseConv3dGrad, ouidx_offset, valid_indices, grad, kernel_size, inchannel, out);
     return out;
 }
+
+
+std::tuple<at::Tensor, at::Tensor> npu_subm_sparse_conv3d_grad_v2(
+    const at::Tensor& features, 
+    const at::Tensor& weight,
+    const at::Tensor& grad_out_features,
+    const at::Tensor& indices_offset
+    )
+{
+    TORCH_CHECK_NPU(features);
+    TORCH_CHECK_NPU(weight);
+    TORCH_CHECK_NPU(grad_out_features);
+    TORCH_CHECK_NPU(indices_offset);
+
+    auto features_size = features.sizes();
+    auto weight_size = weight.sizes();
+
+    at::Tensor features_grad = at::zeros(features_size, features.options());
+    at::Tensor weight_grad = at::zeros(weight_size, weight.options());
+
+    // zero init
+    if (features.options().dtype() == at::kFloat) {
+
+        EXEC_NPU_CMD(aclnnSubmSparseConv3dGradV2, features, weight, grad_out_features, indices_offset, 
+            features_grad, weight_grad);
+
+    } else {
+        at::Tensor weight_grad_fp32 = at::zeros(weight_size, weight.options().dtype(at::kFloat));
+
+        EXEC_NPU_CMD(aclnnSubmSparseConv3dGradV2, features, weight, grad_out_features, indices_offset, 
+            features_grad, weight_grad_fp32);
+
+        weight_grad = weight_grad_fp32.to(at::kHalf);
+    }
+    
+    return std::tie(features_grad, weight_grad);
+}
